dd69691a99a3ed01aab27f2ab9344c113879c5b9
Abhishek2122-star <abhisheklad963@gmail.com>
2026-08-05 13:42:15 +0530

    Build Successfully

--- To-Do-App/src/app/app.ts
+++ To-Do-App/src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Todo } from './todo/todo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [Todo],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('To-Do-App');
-}
+export class App {}
--- To-Do-App/src/app/todo/todo.ts
+++ To-Do-App/src/app/todo/todo.ts
@@ -1,21 +1,26 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-todo',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './todo.html',
   styleUrl: './todo.css',
 })
 export class Todo {
-  task: = '';
+  task: string = '';
   tasks: string[] = [];
 
-  AddTask(){
-    if (this.task.trim()){
-      this.task.push(this task);
+  AddTask() {
+    if (this.task.trim()) {
+      this.tasks.push(this.task.trim());
       this.task = '';
-        
     }
+  }
+
+  DeleteTask(index: number) {
+    this.tasks.splice(index, 1);
   }
 }
